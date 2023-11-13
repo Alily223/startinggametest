5be2e785df6cae9f98865204d4e6bf10ac051469
Austin <112780937+Alily223@users.noreply.github.com>
2023-11-13 14:21:05 -0500

    Bit of Styling Testing

--- src/Pages/NavigationFeatures/Navigation.jsx
+++ src/Pages/NavigationFeatures/Navigation.jsx
@@ -3,18 +3,21 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import {useNavigate} from 'react-router-dom';
 
+import CottageIcon from '@mui/icons-material/Cottage';
+import FactoryIcon from '@mui/icons-material/Factory';
+
 const Navigation = ({money, setMoney}) => {
     const navigate = useNavigate();
     return (
         <div>
             <Stack direction="row" spacing={12} alignItems="center" justifyContent="space-evenly">
-                <Button variant="contained" onClick={() => navigate("/")}>Home</Button>
-                <Button variant="contained" onClick={() => navigate("/factory")}>Factory</Button>
+                <Button onClick={() => navigate("/")}><CottageIcon fontSize='large'/></Button>
+                <Button variant="contained" onClick={() => navigate("/factory")}><FactoryIcon fontSize='small'/>Factory<FactoryIcon fontSize='small'/></Button>
                 <Button variant="contained" onClick={() => navigate("/storage")}>Storage</Button>
                 <Button variant="contained" onClick={() => navigate("/storefront")}>Storefront</Button>
                 <Button variant="contained" onClick={() => navigate("/managers")}>Managers</Button>
                 <Button variant="contained" onClick={() => navigate("/")}>Save/Load</Button>
-                <h1>${money}</h1>
+                <h1 style={{color: 'green'}}>${money}</h1>
             </Stack>
         </div>
     )
